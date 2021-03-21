b699fdf564d95f6397c2a5500bc154e03544cd63
Minh <minhvn29@gmail.com>
2021-03-21 00:57:03 -0700

    Complete static web scraper

--- parser/parser.js
+++ parser/parser.js
@@ -1,6 +1,5 @@
 // sample url and object to test functionality of web scraper
 const url = 'https://www.cbcscomics.com/grading-notes/21-04D753B-004';
-const axios = require('axios');
 const puppeteer = require ('puppeteer');
 
 const parse = async (url) => {
@@ -11,10 +10,20 @@ const parse = async (url) => {
       await page.waitForSelector('body');
 
       await page.goto(url, { waitUntil: 'networkidle0' });
-      const data = await page.evaluate(() => document.querySelector('body * > div.projects').outerHTML);
 
-      console.log(data);
+      // const data = await page.evaluate(() => document.querySelector('body * > div.projects').outerHTML);
+      // console.log(data);
 
+      let comicData = {};
+
+      // functionality to grab required text
+      comicData.id = await page.evaluate(() => document.querySelector('body * > div.input-group > input').value);
+      comicData.title = await page.evaluate(() => document.querySelector('body * > div.projects > h2').innerText);
+      comicData.publisher = await page.evaluate(() => document.querySelector('body * > div.projects > ul > li:first-child').innerText);
+      comicData.grade = await page.evaluate(() => document.querySelector('body * > div.projects > h2:nth-child(5)').innerText);
+      // console.log(comicData);
+
+      return comicData;
     })
     .catch (function (err) {
       console.error (err);
