54e4122369777a6885e8103ce21b74095c911dcd
Minh <minhvn29@gmail.com>
2021-03-21 00:30:29 -0700

    WIP- Grab correct HTML using puppeteer

--- parser/parser.js
+++ parser/parser.js
@@ -1,69 +1,23 @@
-
-console.log('inside parser folder');
-// /* PARSER FUNCTION
-// - should take in id from end of website string
-// - if ID exists in dB, don't add
-// - if ID does NOT exist in db, add to dB using model 
-
-// */
-
-// const axios = require('axios');
-const cheerio = require('cheerio');
-const puppeteer = require('puppeteer');
-
 // sample url and object to test functionality of web scraper
 const url = 'https://www.cbcscomics.com/grading-notes/21-04D753B-004';
+const axios = require('axios');
+const puppeteer = require ('puppeteer');
 
-let comicMetadata = {
-  id: '21-04D753B-004',
-  title: '',
-  publisher: '',
-  grade: ''
-};
+const parse = async (url) => {
+  puppeteer.launch()
+    .then (async browser => { 
+      const page = await browser.newPage(); 	
+      await page.goto(url); 	
+      await page.waitForSelector('body');
 
-const parse = (url, comicMetadata) => {
+      await page.goto(url, { waitUntil: 'networkidle0' });
+      const data = await page.evaluate(() => document.querySelector('body *').outerHTML);
 
+      console.log(data);
 
-  puppeteer
-    .launch()
-    .then(browser => browser.newPage())
-    .then(page => {
-      return page.goto(url).then(function() {
-        return page.content();
-      });
     })
-    .then(html => {
-      const $ = cheerio.load(html);
-      console.log(html);
-
-      const projectDiv = $('div[class="project"]');
-
-      // add specific metadata here:
-      // grab title
-      projectDiv.find('h2').each(function() {
-        console.log('meta b4 title');
-        comicMetadata.title = $(this).text();
-        console.log('meta after', comicMetadata);
-        // newsHeadlines.push({
-        //   title: $(this).text(),
-        // });
-      });
-      // grab publisher
-      // projectDiv.find('ul:nth-child(1)').each(function() {
-      //   comicMetadata.publisher = $(this).text();
-      // });
-
-      // // grab grade
-      // projectDiv.find('nth-child(4)').each(function() {
-      //   comicMetadata.grade = $(this).text();
-      // });
-
-      // console.log('final json object is grabbed as follows: ', comicMetadata);
-      return comicMetadata;
-    })
-    .catch(console.error);
+    .catch (function (err) {
+      console.error (err);
+    });
 }
-
-parse(url, comicMetadata);
-
-module.exports = parse; 
+parse(url);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App.js';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
